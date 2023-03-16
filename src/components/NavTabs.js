89deef76c404b4ef766dcc15ffa92026aec5ee0f
eavalo14 <avaloserick97@gmail.com>
2023-03-15 23:25:55 -0700

    cleaned up react app to mimic functionality of demo

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -16,24 +16,13 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a 
           class="navbar-brand" 
           style={styles.titleName}
-          href="#home"
-          onClick={() => handlePageChange('Home')}
+          href="#about"
+          onClick={() => handlePageChange('About')}
           className={'nav-link'}
         >
           Erick Avalos
         </a>
         <ul className="nav nav-tabs justify-content-end">
-          <li className="nav-item">
-            <a
-              href="#home"
-              onClick={() => handlePageChange('Home')}
-              // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-              // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-              className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-            >
-              Home
-            </a>
-          </li>
           <li className="nav-item">
             <a
               href="#about"
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import '../css/styles.css'
+
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <Card className="text-center text-color mx-auto my-5 py-5"style={{ width: '35rem', background: 'rgba(255,255,255,0.25)' }}>
+      <Card.Body>
+        <Card.Title className="mb-5" > About Me!</Card.Title>
+        <Card.Text>
+        My name is Erick, I am currently 25 years old based out of Los Angeles, Ca. In 2020, I received a bachelors in Electrical Engineering with a specialization in Signals and Systems. 
+        </Card.Text>
+        <Card.Text>
+        I have experience in a variety of fields like robotics, embedded software development, hardware development, blockchain, and web develeopment. 
+        </Card.Text>
+        <Card.Text>
+        I have been a software engineer for about 3 years, and am looking to venture into a full stack role.
+        </Card.Text>
+
+        {/* <Button variant="primary">Go somewhere</Button> */}
+      </Card.Body>
+    </Card>
   );
 }
