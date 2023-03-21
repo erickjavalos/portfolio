dec4e8b1f25bc8b31ada93540eb75c9509a0398a
eavalo14 <avaloserick97@gmail.com>
2023-03-20 22:29:35 -0700

    fixing code and added my resume section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,26 +1,79 @@
 import React from 'react';
 import Header from './components/Header';
+import resume from '../resume/Engineering.pdf'
 
+
+const styles = {
+  title: {
+      width: "100%",
+      fontSize: '2rem',
+      color: 'white',
+  },
+  card : {
+    background: 'rgba(4,22,90,0.5)',
+    width: '55%',
+    borderRadius: '4rem',
+  },
+  text: {
+    marginTop: '1rem',
+    color: 'white',
+    fontSize: '1rem',
+    width: '100%'
+  },
+  experienceTitle: {
+    marginTop: '1rem',
+    color: 'white',
+    fontSize: '1.45rem',
+    width: '100%'
+  },
+  download: {
+    width:'100%',
+    backgroundColor: 'pink'
+  },
+  experiences: {
+    width: '100%',
+    color: 'white'
+  },
+  bullets: {
+    color: 'white',
+    margin: '0px',
+    padding: '0px',
+    textAlign: 'center',
+    fontSize: '.9rem',
+  }
+}
 
 export default function Contact(props) {
   return (
     <>
       <Header currentPage={props.currentPage} handlePageChange={props.handlePageChange}></Header>
-      <div>
-        <h1>Resume Page</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
+      <div className="portfolio-container mx-auto m-5 p-4" style={styles.card}>
+        <h1 className="text-center" style={styles.title}>Resume</h1>
+        <p className="text-center"style={styles.text}>Download my <a href={resume} download="resume.pdf" title="Download">resume</a></p>
+       
+        <h3 className="text-center" style={styles.experienceTitle}>Front End Experience</h3>
+        <ul style={styles.bullets}>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>Responsive Design</li>
+          <li>Bootstrap</li>
+          <li>TailWind</li>
+          <li>React</li>
+        </ul>
+
+        <h3 className="text-center" style={styles.experienceTitle}>Back End Experience</h3>
+        <ul style={styles.bullets}>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>JQuery</li>
+          <li>MySQL, Sequelize</li>
+          <li>MongoDB, Mongoose</li>
+          <li>REST</li>
+          <li>GraphQL</li>
+        </ul>
       </div>
 
     </>
--- src/components/pages/components/AboutCard.js
+++ src/components/pages/components/AboutCard.js
@@ -29,10 +29,10 @@ export default function AboutCard(props) {
         <Card.Img variant="top" src={props.pfp} className="m-3 pfp"style={{width: '150px',height:'150px', borderRadius: '30%'}} />
         {/* text */}
           <Card.Text className='card-text-size m-4' style={styles.descriptionSize}>
-          My name is Erick, I am currently 25 years old based out of Los Angeles, Ca. In 2020, I received a bachelors in Electrical Engineering with a specialization in Signals and Systems. 
+          My name is Erick, I am currently 25 years old based out of Los Angeles, Ca. In 2020, I received a bachelors degree in Electrical Engineering with a specialization in Signals and Systems. 
           </Card.Text>
           <Card.Text className='card-text-size' style={styles.descriptionSize}>
-          I have experience in a variety of fields like robotics, embedded software development, hardware development, blockchain, and web develeopment. 
+          I have experience in a variety of fields such as robotics, embedded software development, hardware development, blockchain, and web develeopment. 
           </Card.Text>
 
           <Card.Text className='card-text-size' style={styles.descriptionSize}>
